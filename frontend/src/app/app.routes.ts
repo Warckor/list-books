c74da955354a7fbc7db9f15904efdb804eb853fc
Miguel Gorostola <miguel.gorostola@soprasteria.com>
2024-12-17 23:32:21 +0100

    Update routing and styling in AppComponent; replace RouterOutlet with RouterModule, add book routes, and refine font-family

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent, title: 'Books' },
+  { path: 'add-book', component: BookFormComponent, title: 'Add Book' },
+];
