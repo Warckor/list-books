c8866c2baf4b4982a141f71613f8eb091029fe9a
Miguel Gorostola <miguel.gorostola@soprasteria.com>
2024-12-17 23:32:01 +0100

    Refactor BooksService to use Angular's inject function and streamline imports

--- frontend/src/app/services/books.service.ts
+++ frontend/src/app/services/books.service.ts
@@ -1,21 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-export interface Book {
-  id?: number;
-  title: string;
-  author: string;
-  published_year?: number;
-}
+import { Book } from '../models/book';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BooksService {
   private apiUrl = `${environment.apiUrl}/books`;
-
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   getBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.apiUrl);
