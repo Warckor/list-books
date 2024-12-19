6704bc318bc29a19b423217e92f633d672fc48f1
Miguel Gorostola <miguel.gorostola@soprasteria.com>
2024-12-19 22:27:20 +0100

    Add CORS support to Flask app and update requirements

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from app.config import Config
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,6 +11,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
